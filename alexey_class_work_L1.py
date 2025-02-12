e7190d1c6a1ed16fc543ed19e4443b08d55f7316
Alexey <gladkiy.alexey@gmail.com>
2025-02-13 00:36:41 +0300

    Add alexey class_home work files

---
+++ alexey_class_work_L1.py
@@ -0,0 +1,173 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello, world!")
+Hello, world!
+
+1 + 1
+2
+print
+<built-in function print>
+Print
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    Print
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+print 123)
+SyntaxError: unmatched ')'
+print(1.2)
+1.2
+print(1,2)
+1 2
+print("Hello\nworld")
+Hello
+world
+print("\tHello\nworld")
+	Hello
+world
+print("hello")
+hello
+print(''hello)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print('hello')
+hello
+print('hello, "world"')
+hello, "world"
+print("hello, \"world\"")
+hello, "world"
+print("\n\n\n")
+
+
+
+
+print ("a","b","c")
+a b c
+print(1,2,3,4,5)
+1 2 3 4 5
+print(1, 2, 3, 4, sep="***")
+1***2***3***4
+print(1,2,3)
+1 2 3
+print()
+
+print(1,2,3, "hello", sep="+", end="***********")
+1+2+3+hello***********
+print("Programming", "Essentials", "in", sep="***", end="...Python")
+Programming***Essentials***in...Python
+print(123)
+123
+print(11_111)
+11111
+print(-6335)
+-6335
+print(2.3)
+2.3
+print(2,3)
+2 3
+print("""hello
+123
+hrlllo
+\n
+23435""")
+hello
+123
+hrlllo
+
+
+23435
+true
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+True
+True
+false
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    false
+NameError: name 'false' is not defined. Did you mean: 'False'?
+False
+False
+type(1)
+<class 'int'>
+type("f")
+<class 'str'>
+type(True)
+<class 'bool'>
+print("""
+"I`m"
+""Learning""
+\"""Python\"""
+""")
+
+"I`m"
+""Learning""
+"""Python"""
+
+name = "alexey"
+print(name)
+alexey
+1cat
+SyntaxError: invalid decimal literal
+&asd
+SyntaxError: invalid syntax
+66fgh
+SyntaxError: invalid decimal literal
+my dot
+SyntaxError: invalid syntax
+Name = "FFff"
+print(Name)
+FFff
+print(name)
+alexey
+name  = 999
+print(999)
+999
+print(name)
+999
+name = "stef"
+print(name)
+stef
+name = 'stef'
+>>> print(name)
+stef
+>>> x =10
+>>> x = x + 5
+>>> x +=5
+>>> print(x)
+20
+>>> x = 9
+>>> y = 8
+>>> c = 99
+>>> z = "adasd"
+>>> print(x,y,c,z)
+9 8 99 adasd
+>>> result = x+y+c
+>>> result
+116
+>>> a, b, c = 9, 6 ,7
+>>> a
+9
+>>> b
+6
+>>> c
+7
+>>> q=w=e=r=t=8
+>>> q
+8
+w
+>>> 
+>>> e
+8
+>>> r
+8
+>>> t
+8
+>>> 2 * (4/5) + 6
+7.6
+>>> john  = 1
+>>> mary = 2
+>>> adam = 1
+>>> total_apples = john + mary + adam
+>>> print(total_apples)
+4
