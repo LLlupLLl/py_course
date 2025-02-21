7a8b3643126ea0313bf97b0184b51b3f08f72bd4
Alexey <gladkiy.alexey@gmail.com>
2025-02-21 13:42:40 +0300

    Add files lecture 1

---
+++ Lecture 1/alexey_home_work_L1.py
@@ -0,0 +1,16 @@
+""" # Home work Lecture 1
+
+# Home word 1.2
+print("    *", "   * *", "  *   *", " *     *",
+      "***   ***", "  *   *",
+      "  *   *", "  *****", sep="\n")  # Вывод стрелки.
+
+# Home work 1.5
+kilometers = 12.25
+miles = 7.38
+
+print(miles, "miles is", round((miles * 1.61), 2),
+     "kilometers")  # Расчет километров.
+print(kilometers, "kilometers is", round((kilometers / 1.61), 2),
+      "miles")  # Расчет миль.
+ """
